6938e70ec0b7a3ef1db4ec67c3018213f00c9dae
Jean Woodly Estime <100792438+jeanzinho509@users.noreply.github.com>
2023-09-18 22:22:56 -0300

    Add files via upload

---
+++ aula1/Aluno.java
@@ -0,0 +1,50 @@
+package aula2808;
+
+public class Aluno {
+	
+	    private int id;
+	    private String nome;
+	    private int idade;
+		public Aluno(int i, String string, int j) {
+			
+		}
+		/**
+		 * @return the id
+		 */
+		public int getId() {
+			return id;
+		}
+		/**
+		 * @param id the id to set
+		 */
+		public void setId(int id) {
+			this.id = id;
+		}
+		/**
+		 * @return the nome
+		 */
+		public String getNome() {
+			return nome;
+		}
+		/**
+		 * @param nome the nome to set
+		 */
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+		/**
+		 * @return the idade
+		 */
+		public int getIdade() {
+			return idade;
+		}
+		/**
+		 * @param idade the idade to set
+		 */
+		public void setIdade(int idade) {
+			this.idade = idade;
+		}
+
+	    // Construtor, getters e setters
+	}
+
---
+++ aula1/Cliente.java
@@ -0,0 +1,69 @@
+package aula2808;
+
+public class Cliente {
+	private String cpf;
+	private String nome;
+	private String dataNascimento;
+	private String sexo;
+	private String temFilho;
+	/**
+	 * @return the cpf
+	 */
+	public String getCpf() {
+		return cpf;
+	}
+	/**
+	 * @param cpf the cpf to set
+	 */
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	/**
+	 * @return the nome
+	 */
+	public String getNome() {
+		return nome;
+	}
+	/**
+	 * @param nome the nome to set
+	 */
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	/**
+	 * @return the dataNascimento
+	 */
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+	/**
+	 * @param dataNascimento the dataNascimento to set
+	 */
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	/**
+	 * @return the sexo
+	 */
+	public String getSexo() {
+		return sexo;
+	}
+	/**
+	 * @param sexo the sexo to set
+	 */
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	/**
+	 * @return the temFilho
+	 */
+	public String getTemFilho() {
+		return temFilho;
+	}
+	/**
+	 * @param temFilho the temFilho to set
+	 */
+	public void setTemFilho(String temFilho) {
+		this.temFilho = temFilho;
+	} 	
+}
