e679bad93405343ca637806b45933c64be9469d2
Jean Woodly Estime <100792438+jeanzinho509@users.noreply.github.com>
2023-10-02 21:50:58 -0300

    Add files via upload

---
+++ src/TiposPessoa/PessoaFisica.java
@@ -0,0 +1,21 @@
+package TiposPessoa;
+
+import Repositorio.BasePessoa;
+
+
+public class PessoaFisica extends BasePessoa {
+    private int cpf;
+
+    public PessoaFisica(String nome, int cpf) {
+        super(nome);
+        this.cpf = cpf;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+}
---
+++ src/TiposPessoa/PessoaJuridica.java
@@ -0,0 +1,20 @@
+package TiposPessoa;
+
+import Repositorio.BasePessoa;
+
+public class PessoaJuridica extends BasePessoa {
+    private String cnpj;
+
+    public PessoaJuridica(String nome, String cnpj) {
+        super(nome);
+        this.cnpj = cnpj;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+}
